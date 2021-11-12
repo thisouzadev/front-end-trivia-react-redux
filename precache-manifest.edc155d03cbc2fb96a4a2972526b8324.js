ff482f3a97c6365bb07b33668f5b9a75de0d40a6
Thiago Souza <78744052+thisouzadev@users.noreply.github.com>
2021-11-12 08:50:18 -0300

    Updates

---
+++ precache-manifest.edc155d03cbc2fb96a4a2972526b8324.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b357e0df6617cf04c8b4620475364a8",
+    "url": "/trivia/index.html"
+  },
+  {
+    "revision": "3afade16b9725755e86e",
+    "url": "/trivia/static/css/main.93c29e5d.chunk.css"
+  },
+  {
+    "revision": "6a22985cb61f4846486a",
+    "url": "/trivia/static/js/2.42ceb002.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/trivia/static/js/2.42ceb002.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3afade16b9725755e86e",
+    "url": "/trivia/static/js/main.27063e95.chunk.js"
+  },
+  {
+    "revision": "8cf3ddda99dc054ff6df",
+    "url": "/trivia/static/js/runtime-main.55dca74b.js"
+  },
+  {
+    "revision": "466d153e266e6e813c5ef2c55aa5fc2c",
+    "url": "/trivia/static/media/trivia.466d153e.png"
+  }
+]);
